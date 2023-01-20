6d836badbf3c9d622bdbe505c4480302bfe54839
Natanna <natannacortez@gmail.com>
2023-01-19 22:23:48 -0300

    Updating car

--- src/main/java/com/modelaai/api/CarController.java
+++ src/main/java/com/modelaai/api/CarController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,5 +39,19 @@ public class CarController {
     @DeleteMapping("/{id}")
     public void deleteCar(@PathVariable Long id){
         repository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCar(@PathVariable Long id,@RequestBody @Valid CarDTO req){
+        repository.findById(id).map(cars -> {
+            cars.setModelo(req.modelo());
+            cars.setFabricante(req.fabricante());
+            cars.setDataFabricacao(req.dataFabricacao());
+            cars.setValor(req.valor());
+            cars.setAnoModelo(req.anoModelo());
+
+            return repository.save(cars);
+        });
+
     }
 }
